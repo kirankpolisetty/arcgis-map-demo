c3891d6ca7e0e3c7bd32e4b91ba5ee82bf2c330d
Kiran polisetty <kirankpolisetty@gmail.com>
2025-08-20 21:26:15 +0300

    reformatted fro test suite changes

--- src/app/aarcgis-map/aarcgis-map.component.spec.ts
+++ src/app/aarcgis-map/aarcgis-map.component.spec.ts
@@ -1,39 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { Rig } from '../water-well';
 import { ArcgisMapComponent } from './aarcgis-map.component';
 
-describe('AarcgisMapComponent', () => {
+describe('ArcgisMapComponent (real HTTP)', () => {
   let component: ArcgisMapComponent;
   let fixture: ComponentFixture<ArcgisMapComponent>;
-  let httpMock: HttpTestingController;
-
-  const mockRigs: Rig[] = [
-    { rigId: 'R1', lat: 24.5, lng: 48.2, location: 'Loc1', classification: 'A' },
-    { rigId: 'R2', lat: 24.6, lng: 48.3, location: 'Loc2', classification: 'B' },
-  ];
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [ArcgisMapComponent, HttpClientTestingModule],
+      imports: [ArcgisMapComponent, HttpClientModule], // use real HttpClient
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ArcgisMapComponent);
     component = fixture.componentInstance;
-    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
-  afterEach(()=> {
-    httpMock.verify();
-  })
-
-  it('should create the component.. ', ()=> {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-
+  it('should load rigs from assets/rigs.json', waitForAsync(() => {
+    // Wait a bit for ngOnInit to fetch the JSON
+    fixture.whenStable().then(() => {
+      expect(component.rigs.length).toBeGreaterThan(0);
+      console.log('Loaded rigs:', component.rigs);
+    });
+  }));
 });
